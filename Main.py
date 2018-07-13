86cabb1b2b531b22a5b1f66cdd37515167703381
mostafax <mostafamousa1080p@gmail.com>
2018-07-13 10:40:36 +0200

    Adiing Some Normilzation

--- Main.py
+++ Main.py
@@ -73,6 +73,45 @@ FC_Biases = {
 
 }
 
+
+def Normlization(Value,MinVale,MaxValue):
+    '''
+    normalize the Input
+    :param value: pixl value
+    :param MinVale:Old min Vale
+    :param MaxValue: Old Max vale
+    :return: Normailed Input between 0 1
+    '''
+    MinNormalizeValue = 0
+    MaxNormalizeVale = 1
+    Value = MinNormalizeValue + (((MaxNormalizeVale-MinNormalizeValue)*(Value- MinVale))/(MaxValue-MinVale))
+    return Value
+
+def DeNormlization(Value,MinVale,MaxValue):
+    '''
+    normalize the Input
+    :param value: pixl value
+    :param MinVale:Old min Vale
+    :param MaxValue: Old Max vale
+    :return: Normailed Input between 128 -128
+    '''
+    MinNormalizeValue = -128
+    MaxNormalizeVale = 128
+    Value = MinNormalizeValue + (((MaxNormalizeVale-MinNormalizeValue)*(Value- MinVale))/(MaxValue-MinVale))
+    return Value
+
+def F_Norm(Tens):
+    '''
+    Caclutes the Frobenius Normalization using this Formula
+    sqrt(sum(each values**2))
+    #https://en.wikipedia.org/wiki/Frobenius_normal_form
+
+    :param Tens: tf tensor
+    :return: Frobenius Normalization of the tensor
+    '''
+    return tf.reduce_sum(input_tensor=Tens**2)**0.5
+
+
 def Construct_Graph(input):
     lowconv1 = tf.nn.relu(tf.nn.conv2d(input=input, filter=Low_Weight['wl1'], strides=[1, 2, 2, 1], padding='SAME') + Low_Biases['bl1'])
     lowconv2 = tf.nn.relu(tf.nn.conv2d(input=lowconv1, filter=Low_Weight['wl2'], strides=[1, 1, 1, 1], padding='SAME') + Low_Biases['bl2'])
@@ -83,7 +122,6 @@ def Construct_Graph(input):
     #Mid
     midconv1=tf.nn.relu(tf.nn.conv2d(input=lowconv6,filter=Mid_Weight['wm1'],strides=[1,1,1,1],padding='SAME')+Mid_Biases['bm1'])
     midconv2=tf.nn.relu(tf.nn.conv2d(input=midconv1,filter=Mid_Weight['wm2'],strides=[1,1,1,1],padding='SAME')+Mid_Biases['bm2'])
-
 
 
     #Global
@@ -111,3 +149,6 @@ def Construct_FC(global_cnn_output):
     features=tf.nn.relu(features)
 
     return features
+
+
+
