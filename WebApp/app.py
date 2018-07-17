4e0ecc3f5374a535ebb4098c33f6686816fb38e6
mostafax <mostafamousa1080p@gmail.com>
2018-07-17 12:17:13 +0200

    Adding the web app

---
+++ WebApp/app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'static/')
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        filename = file.filename
+        destination = "/".join([target, filename])
+        file.save(destination)
+#    print(filename)
+    return render_template("complete.html",value=filename)
+
+if __name__ == "__main__":
+    app.run()
