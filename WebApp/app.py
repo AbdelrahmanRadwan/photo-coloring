3f09364f084b8ffaeda1d00b52410518d4b9b8c9
mostafax <mostafamousa1080p@gmail.com>
2018-07-21 00:51:35 +0200

    Adding Documentation

--- WebApp/app.py
+++ WebApp/app.py
@@ -3,28 +3,43 @@ from flask import (Flask,
                    render_template,
                    request)
 
-from temp import dummyfun
+from temp import dummy_fun
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
 def delete_images(folder):
-    for the_file in os.listdir(folder):
-        file_path = os.path.join(folder, the_file)
+    """
+    Delete Images in Specific folder given the Path
+    :param folder: The Folder Path
+    """
+    for the_file_path in os.listdir(folder):
+        file_path = os.path.join(folder, the_file_path)
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
         except Exception as e:
-            print(e) #Rabna ma ygyb Erorr
+            print(e)
+
 
 @app.route("/")
+@app.route('/Home')
+@app.route('/home')
 def index():
-    #delete_images(os.path.join(APP_ROOT, 'static/'))
+    """
+    :return: Render the main Page
+    """
+    delete_images(os.path.join(APP_ROOT, 'static/'))
     return render_template("upload.html")
 
 
-@app.route("/upload", methods=['POST','GET'])
-def upload():
+@app.route("/upload", methods=['POST'])
+def upload_image():
+    """
+    Upload Images  to the Static Folder
+    :return:
+    """
     global destination
     target = os.path.join(APP_ROOT, 'static/')
     # delete_images(target)
@@ -36,13 +51,15 @@ def upload():
         destination = "/".join([target, filename])
         file.save(destination)
 
-#    print(filename)
-    dummyfun(destination)
+    dummy_fun(target+filename)
     return render_template("complete.html", value=filename)
 
 
 @app.route('/gallery')
-def get_gallery():
+def display_all_images():
+    """
+    :return: Render all Images in Static Folder
+    """
     image_names = os.listdir('static/')
     print(image_names)
     return render_template("gallery.html", image_names=image_names)
